e55ba4421bce278e78db34e07640bc57523bf822
sanisaha <44751151+sanisaha@users.noreply.github.com>
2022-11-09 10:49:58 +0200

    banner setup

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import background from '../../Assets/Banner Images/packing-13.png'
 
 const Home = () => {
     return (
         <div>
-            <h1>This is home</h1>
+            <div className="hero min-h-screen" style={{ backgroundImage: `url(${background})` }}>
+                <div className="hero-overlay bg-opacity-60"></div>
+                <div className="hero-content text-center text-neutral-content">
+                    <div className="max-w-md">
+                        <h1 className="mb-5 text-5xl font-bold">Moving is now easy and stressfree with CarryYou</h1>
+                        <p className="mb-5">CarryYou value your presious time and do really care about your very own important household belongigs </p>
+                        <button className="btn btn-primary"><Link>Book Your Service Now</Link></button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
