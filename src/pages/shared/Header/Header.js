d4410963d2c94e9f39ca2720eeda3201fda6dcd8
sanisaha <44751151+sanisaha@users.noreply.github.com>
2022-11-09 20:42:33 +0200

    conditional rendering set for user loggedin

--- src/pages/shared/Header/Header.js
+++ src/pages/shared/Header/Header.js
@@ -1,10 +1,13 @@
-import { FOCUSABLE_SELECTOR } from '@testing-library/user-event/dist/utils';
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
-import { FaBeer } from 'react-icons/fa';
 import logo from '../../../Assets/website logo/carryYou.jpg'
+import { AuthContext } from '../../../Context/AuthProvider/AuthProvider';
 
 const Header = () => {
+    const { user, logOut } = useContext(AuthContext);
+    const handleLogOut = () => {
+        logOut();
+    }
     const navbarItems = <>
         <li className='font-semibold'><Link>My reviews</Link></li>
         <li className='font-semibold'><Link>Add Service</Link></li>
@@ -18,7 +21,12 @@ const Header = () => {
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
                     <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                        {navbarItems}
+                        {
+                            user?.uid ?
+                                <>{navbarItems}</>
+                                :
+                                <></>
+                        }
                     </ul>
                 </div>
                 <div className='flex items-center'>
@@ -29,12 +37,20 @@ const Header = () => {
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal p-0">
                     {
-                        navbarItems
+                        user?.uid ?
+                            <>{navbarItems}</>
+                            :
+                            <></>
                     }
                 </ul>
             </div>
             <div className="navbar-end">
-                <Link to='/register'><button className="btn">SignUp</button></Link>
+                {
+                    user?.uid ?
+                        <><Link onClick={handleLogOut}><button className="btn">LogOut</button></Link></>
+                        :
+                        <><Link to='/register'><button className="btn">SignUp</button></Link></>
+                }
             </div>
         </div>
     );
