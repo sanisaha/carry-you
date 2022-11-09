b5f5a3cafbc9b97af9cc6cb71d7488dc104c6624
sanisaha <44751151+sanisaha@users.noreply.github.com>
2022-11-09 20:58:00 +0200

    photoview set up for images

--- src/pages/Home/Services/ServiceCard.js
+++ src/pages/Home/Services/ServiceCard.js
@@ -1,28 +1,32 @@
 import React from 'react';
+import 'react-photo-view/dist/react-photo-view.css';
+import { PhotoProvider, PhotoView } from 'react-photo-view';
 import { Link } from 'react-router-dom';
 
 const ServiceCard = ({ service }) => {
     const { _id, img, price, title, details } = service;
     return (
-        <div className="card w-full bg-base-100 shadow-xl">
-            <figure><img src={img} alt="Shoes" /></figure>
-            <div className="card-body">
-                <h2 className="card-title">
-                    {title}
-                </h2>
-                <p>
-                    {details.length > 100 ?
-                        <>{details.slice(0, 100) + '.....'}</>
-                        :
-                        details
-                    }
-                </p>
-                <p>Price: ${price}</p>
-                <div className="card-actions justify-end">
-                    <Link to={`/services/${_id}`} className='btn badge badge-outline'>Show details</Link>
+        <PhotoProvider>
+            <div className="card w-full bg-base-100 shadow-xl">
+                <PhotoView src={img}><figure><img src={img} alt="Shoes" /></figure></PhotoView>
+                <div className="card-body">
+                    <h2 className="card-title">
+                        {title}
+                    </h2>
+                    <p>
+                        {details.length > 100 ?
+                            <>{details.slice(0, 100) + '.....'}</>
+                            :
+                            details
+                        }
+                    </p>
+                    <p>Price: ${price}</p>
+                    <div className="card-actions justify-end">
+                        <Link to={`/services/${_id}`} className='btn badge badge-outline'>Show details</Link>
+                    </div>
                 </div>
             </div>
-        </div>
+        </PhotoProvider>
     );
 };
 
--- src/pages/Home/Services/ServiceDetailCard.js
+++ src/pages/Home/Services/ServiceDetailCard.js
@@ -1,28 +1,32 @@
 import React from 'react';
 import { Link, useLoaderData } from 'react-router-dom';
+import 'react-photo-view/dist/react-photo-view.css';
+import { PhotoProvider, PhotoView } from 'react-photo-view';
 
 const ServiceDetailCard = () => {
     const service = useLoaderData();
     const { _id, img, price, title, details } = service;
     return (
-        <div>
+        <PhotoProvider>
             <div>
-                <div className="hero min-h-screen bg-base-200">
-                    <div className="hero-content flex-col lg:flex-row">
-                        <img src={img} alt='' className="max-w-lg rounded-lg shadow-2xl" />
-                        <div>
-                            <h1 className="text-5xl font-bold">{title}</h1>
-                            <p className="pt-6 text-lg">{details}</p>
-                            <p className="py-2 text-xl text-secondary">Price of the service :${price}</p>
-                            <button className="btn btn-primary"><Link to='/login'>Login Now</Link></button>
+                <div>
+                    <div className="hero min-h-screen bg-base-200">
+                        <div className="hero-content flex-col lg:flex-row">
+                            <PhotoView src={img}><img src={img} alt='' className="max-w-lg rounded-lg shadow-2xl" /></PhotoView>
+                            <div>
+                                <h1 className="text-5xl font-bold">{title}</h1>
+                                <p className="pt-6 text-lg">{details}</p>
+                                <p className="py-2 text-xl text-secondary">Price of the service :${price}</p>
+                                <button className="btn btn-primary"><Link to='/login'>Login Now</Link></button>
+                            </div>
                         </div>
                     </div>
                 </div>
+                <div>
+                    <h1>Review section</h1>
+                </div>
             </div>
-            <div>
-                <h1>Review section</h1>
-            </div>
-        </div>
+        </PhotoProvider>
     );
 };
 
